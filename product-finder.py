326ffe23f59e4b6cfb78066f990268a9a74c567e
DanyilNiemtsov <dniemtsov@gmail.com>
2023-10-12 19:08:05 +1300

    initial commit

---
+++ product-finder.py
@@ -0,0 +1,18 @@
+Enter_Password = int(input("Enter password: "))
+
+if Enter_Password == 0000:
+    while True:
+        Enter_number = input("Enter number: ")
+
+        if Enter_number == int(1):
+            print("Details")
+            print("Description")
+        #add more as needed
+        else:
+            break
+        
+        
+
+    
+
+
